3990c08e09f7e50aca30d81ae75efda33e7d2d6f
Abir Abid <abir.abid@proxym-it.com>
2020-06-04 22:13:43 +0200

    commit 04/06/2020 filterTable

--- src/main/resources/static/js_files/search_filter.js
+++ src/main/resources/static/js_files/search_filter.js
@@ -1,3 +1,4 @@
+/********recherche memebres******/
 $(document).ready(function () {
 
 
@@ -73,6 +74,8 @@ $(document).ready(function () {
 
 });
 
+/********recherche events******/
+
 $(document).ready(function () {
 
 
@@ -103,6 +106,56 @@ $(document).ready(function () {
             {"orderable": false, "targets": 4},
             {"orderable": false, "targets": 5},
             {"orderable": false, "targets": 6}
+
+
+        ]
+    });
+
+    // Apply the search
+    otable.columns().every(function () {
+
+        var that = this;
+        $('input', this.footer()).on('keyup change', function () {
+            if (that.search() !== this.value) {
+                that
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+
+});
+/********recherche sponsors******/
+$(document).ready(function () {
+
+
+    $('#tableSponsor tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder=" ' + title + '"  size="6"  class="form-control"/>');
+    });
+
+    // DataTables est un plug-in jQuery open-source permettant de dynamiser un tableau HTML
+    var otable = $('#tableSponsor').DataTable({
+        "pagingType": "simple_numbers",
+        "language": {
+            "paginate": {
+                "next": "Suivant",
+                "previous": "Précédent"
+            },
+            "search": "<strong> Rechercher </strong>",
+        },
+        "bLengthChange": false,
+        "bFilter": true,
+        "bInfo": false,
+        "bAutoWidth": false,
+
+        "fnInitComplete": function (oSettings) {
+            oSettings.oLanguage.sZeroRecords = "<center><span class=\"alert alert-secondary\" >Désolé, votre recherche n'a retourné aucun résultat.</span> </center>"
+        },
+        "columnDefs": [
+            {"orderable": false, "targets": 3},
+            {"orderable": false, "targets": 4}
 
 
         ]
