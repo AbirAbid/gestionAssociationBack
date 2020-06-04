b9daec2f721e1b454d441d070774c80a358ea9c9
Abir Abid <abir.abid@proxym-it.com>
2020-06-04 23:00:30 +0200

    commit 04/06/2020 filterTable

--- src/main/resources/static/js_files/search_filter.js
+++ src/main/resources/static/js_files/search_filter.js
@@ -205,7 +205,58 @@ $(document).ready(function () {
             oSettings.oLanguage.sZeroRecords = "<center><span class=\"alert alert-secondary\" >Désolé, votre recherche n'a retourné aucun résultat.</span> </center>"
         },
         "columnDefs": [
-            {"orderable": false, "targets": 4}
+            {"orderable": false, "targets": 5}
+
+
+        ]
+    });
+
+    // Apply the search
+    otable.columns().every(function () {
+
+        var that = this;
+        $('input', this.footer()).on('keyup change', function () {
+            if (that.search() !== this.value) {
+                that
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+
+});
+/*****recherche bénévoles ****/
+
+$(document).ready(function () {
+
+
+    $('#tableBenevole tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder=" ' + title + '"  size="6"  class="form-control"/>');
+    });
+
+    // DataTables est un plug-in jQuery open-source permettant de dynamiser un tableau HTML
+    var otable = $('#tableBenevole').DataTable({
+        "pagingType": "simple_numbers",
+        "language": {
+            "paginate": {
+                "next": "Suivant",
+                "previous": "Précédent"
+            },
+            "search": "<strong> Rechercher </strong>",
+        },
+        "bLengthChange": false,
+        "bFilter": true,
+        "bInfo": false,
+        "bAutoWidth": false,
+
+        "fnInitComplete": function (oSettings) {
+            oSettings.oLanguage.sZeroRecords = "<center><span class=\"alert alert-secondary\" >Désolé, votre recherche n'a retourné aucun résultat.</span> </center>"
+        },
+        "columnDefs": [
+            {"orderable": false, "targets": 6},
+            {"orderable": false, "targets": 7}
 
 
         ]
