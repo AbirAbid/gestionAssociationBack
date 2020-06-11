4db98a2c1f75b399f2b9aca229912000dd542444
Abir Abid <abir.abid@proxym-it.com>
2020-06-11 15:02:46 +0200

    commit 11/06/2020 stat

--- src/main/resources/static/js_files/statistic.js
+++ src/main/resources/static/js_files/statistic.js
@@ -1,97 +1,112 @@
-
 var app = angular.module('plunker', ['nvd3']);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', ['$scope', 'services', function($scope, services) {
+
+    services.getData().then(function successCb(data) {
+        $scope.data = _.map(data.data, function(prod) {
+            return {
+                key: prod.categorie,
+                y: prod.count
+            };
+        });
+    });
+
     $scope.options = {
         chart: {
-            type: 'lineChart',
+            type: 'pieChart',
+            height: 500,
+            x: function(d){return d.key;},
+            y: function(d){return d.y;},
+            showLabels: true,
+            duration: 500,
+            labelThreshold: 0.01,
+            labelSunbeamLayout: true,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 35,
+                    bottom: 5,
+                    left: 0
+                }
+            }
+        }
+    };
+
+}])
+
+    .factory('services', ['$http', function($http){
+        var object = {};
+        object.getData = function(){
+            return $http.get('http://localhost:8080/countEventCategories');
+        };
+        return object;
+    }]);
+
+app.controller('MainCtrl2', function($scope) {
+    $scope.options = {
+        chart: {
+            type: 'discreteBarChart',
             height: 450,
             margin : {
                 top: 20,
                 right: 20,
-                bottom: 40,
+                bottom: 50,
                 left: 55
             },
-            x: function(d){ return d.x; },
-            y: function(d){ return d.y; },
-            useInteractiveGuideline: true,
-            dispatch: {
-                stateChange: function(e){ console.log("stateChange"); },
-                changeState: function(e){ console.log("changeState"); },
-                tooltipShow: function(e){ console.log("tooltipShow"); },
-                tooltipHide: function(e){ console.log("tooltipHide"); }
+            x: function(d){return d.label;},
+            y: function(d){return d.value + (1e-10);},
+            showValues: true,
+            valueFormat: function(d){
+                return d3.format(',.4f')(d);
             },
+            duration: 500,
             xAxis: {
-                axisLabel: 'Time (ms)'
+                axisLabel: 'X Axis'
             },
             yAxis: {
-                axisLabel: 'Voltage (v)',
-                tickFormat: function(d){
-                    return d3.format('.02f')(d);
-                },
+                axisLabel: 'Y Axis',
                 axisLabelDistance: -10
-            },
-            callback: function(chart){
-                console.log("!!! lineChart callback !!!");
-            }
-        },
-        title: {
-            enable: true,
-            text: 'Title for Line Chart'
-        },
-        subtitle: {
-            enable: true,
-            text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-            css: {
-                'text-align': 'center',
-                'margin': '10px 13px 0px 7px'
-            }
-        },
-        caption: {
-            enable: true,
-            html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-            css: {
-                'text-align': 'justify',
-                'margin': '10px 13px 0px 7px'
             }
         }
     };
 
-    $scope.data = sinAndCos();
-
-    /*Random Data Generator */
-    function sinAndCos() {
-        var sin = [],sin2 = [],
-            cos = [];
-
-        //Data is represented as an array of {x,y} pairs.
-        for (var i = 0; i < 100; i++) {
-            sin.push({x: i, y: Math.sin(i/10)});
-            sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-            cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
+    $scope.data = [
+        {
+            key: "Cumulative Return",
+            values: [
+                {
+                    "label" : "A" ,
+                    "value" : -29.765957771107
+                } ,
+                {
+                    "label" : "B" ,
+                    "value" : 0
+                } ,
+                {
+                    "label" : "C" ,
+                    "value" : 32.807804682612
+                } ,
+                {
+                    "label" : "D" ,
+                    "value" : 196.45946739256
+                } ,
+                {
+                    "label" : "E" ,
+                    "value" : 0.19434030906893
+                } ,
+                {
+                    "label" : "F" ,
+                    "value" : -98.079782601442
+                } ,
+                {
+                    "label" : "G" ,
+                    "value" : -13.925743130903
+                } ,
+                {
+                    "label" : "H" ,
+                    "value" : -5.1387322875705
+                }
+            ]
         }
-
-        //Line chart data should be sent as an array of series objects.
-        return [
-            {
-                values: sin,      //values - represents the array of {x,y} data points
-                key: 'Sine Wave', //key  - the name of the series.
-                color: '#ff7f0e',  //color - optional: choose your own line color.
-                strokeWidth: 2,
-                classed: 'dashed'
-            },
-            {
-                values: cos,
-                key: 'Cosine Wave',
-                color: '#2ca02c'
-            },
-            {
-                values: sin2,
-                key: 'Another sine wave',
-                color: '#7777ff',
-                area: true      //area - set to true if you want this line to turn into a filled area chart.
-            }
-        ];
-    };
+    ]
 });
-
